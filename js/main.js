1bf4af7e01fd66587a1a9432865d9a8d0e181d63
Carlo Angelo Cordoviz <carloangelo.cordoviz@asurion.com>
2016-08-31 17:56:49 +0800

    Test encrypted gcm data

--- js/main.js
+++ js/main.js
@@ -58,12 +58,12 @@ if ('serviceWorker' in navigator) {
                 {
                     userVisibleOnly: true
                 }
-            ).then(function(subscriptionObj) {
-                    var rawKey = subscriptionObj.getKey ? subscriptionObj.getKey('p256dh') : '';
+            ).then(function(subscription) {
+                    var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
                     key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
-                    var rawAuthSecret = subscriptionObj.getKey ? subscriptionObj.getKey('auth') : '';
+                    var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
                     authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : '';
-                    endpoint = subscriptionObj.endpoint;
+                    endpoint = subscription.endpoint;
 
                     /*console.log("Endpoint: " + endpoint);
                     console.log("Key: " + key);
