2857a1479648dcef5a9f402289d76e0b0fe62558
Carlo Angelo Cordoviz <carloangelo.cordoviz@asurion.com>
2016-09-01 17:46:14 +0800

    Open PWA from service worker

--- js/main.js
+++ js/main.js
@@ -4,17 +4,22 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js', {scope: '/'})
         .then(function(registrationObj) {
         console.log('sw.js registered. ', registrationObj);
+            alert("REGISTERED");
     }).catch(function(error) {
+            alert("ERROR");
         console.log('Error: ', error);
     });
 
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        alert("READY");
         serviceWorkerRegistration.pushManager.getSubscription()
             .then(function(subscription) {
+                alert("GETSUBSCRIPTION");
                 $(".loading-overlay").removeClass("hidden");
                 $(".mdl-card").hide(); // hide all cards
 
                 if (!subscription) {
+                    alert("NOSUBSCRIPTION");
                     // We aren’t subscribed to push, so set UI
                     // to allow the user to enable push
                     $(".subscription-card").show();
@@ -31,6 +36,7 @@ if ('serviceWorker' in navigator) {
             })
             .catch(function(err) {
                 console.log('Error during getSubscription()', err);
+                alert("ERRORGETSUBSCRIPTION");
                 $(".loading-overlay").addClass("hidden");
             });
 
