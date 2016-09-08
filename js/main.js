b03f265220249fd9cfe34e6163f3d66209809f96
Carlo Angelo Cordoviz <carloangelo.cordoviz@asurion.com>
2016-09-08 22:33:14 +0800

    Integrate REST API

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker is supported');
 
-    $(".teamcast-pwa.mdl-layout").show();
+    $(".teamcast-pwa.mdl-layout").removeClass("invisible");
     $(document).ready(function() {
         var controller = navigator.serviceWorker.controller;
 
@@ -308,5 +308,5 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     $("header, footer").remove();
     $(".loading-overlay").addClass("hidden");
     $(".not-supported-card").show();
-    $(".teamcast-pwa.mdl-layout").show();
+    $(".teamcast-pwa.mdl-layout").removeClass("invisible");
 }
